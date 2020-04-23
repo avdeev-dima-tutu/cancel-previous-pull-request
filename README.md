458de86dba6844c132765498ab264a038071884c
avdim <avdim@mail.ru>
2020-04-23 03:08:48 +0300

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+##Usage
+```yaml
+name: Pull Request Cancel Example
+on: [pull_request]
+
+jobs:
+  check-job:
+    runs-on: ubuntu-latest
+    steps:
+      - run: echo 'Running now'
+      - run: sleep 10
+      - run: sleep 10
+      - run: sleep 10
+      - uses: tutu-ru-mobile/cancel-previous-pull-request@tag1.0.0
+        with:
+          access_token: "${{ secrets.GITHUB_TOKEN }}"
+      - run: sleep 10
+      - run: sleep 10
+      - run: sleep 10
+      - run: echo 'wasnt cancelled'
+        
+```
+
+## Contributing
+- Run `yarn install`
+- Edit `./src/index.ts`
+- Run `yarn build`
+- Commit changes including `./dist/index.js` bundle
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+if (!github) {
+    throw new Error('Module not found: github');
+}
+
+if (!core) {
+    throw new Error('Module not found: core');
+}
+
+async function main() {
+    console.log("hello TypeScript")
+    const {repo: {owner, repo}} = github.context;
+    if (!github.context.payload.pull_request) {
+        console.error("Action available only at pull_request")
+        return
+    }
+    const branch = github.context.payload.pull_request.head.ref;
+
+    // console.log({eventName, sha, headSha, branch, owner, repo});
+    const token = core.getInput('access_token', {required: true});
+    console.log(`Found token: ${token ? 'yes' : 'no'}`);
+    const octokit = new github.GitHub(token);
+
+    const allWorkflows = await octokit.actions.listRepoWorkflows({
+        owner: owner,
+        repo: repo
+    })
+    const workflow = allWorkflows.data.workflows.find(wf => wf.name === github.context.workflow)
+    if (workflow === undefined) {
+        console.error(`can't find workflow ${github.context.workflow}`)
+        return
+    }
+    const runs = (await octokit.actions.listWorkflowRuns({
+        owner,
+        repo,
+        workflow_id: workflow.id,
+        branch
+    })).data;
+
+    const pull_request = github.context.payload.pull_request
+    const maxRunNumber = Math.max.apply(Math, runs.workflow_runs.map(run => run.run_number))
+    const runningWorkflows = runs.workflow_runs.filter(
+        run =>
+            // run.head_sha !== pull_request.head.sha && // PR смотрит на другой коммит
+            run.status !== 'completed' &&  // PR не собран
+            run.pull_requests.some(pr => pr.number === pull_request.number) &&
+            run.run_number < maxRunNumber
+    );
+    console.log(`Found ${runningWorkflows.length} runs in progress.`);
+    for (const {id, head_sha, status} of runningWorkflows) {
+        try {
+            console.log('Cancelling another run: ', {id, head_sha, status});
+            const res = await octokit.actions.cancelWorkflowRun({
+                owner,
+                repo,
+                run_id: id
+            });
+            console.log(`Status ${res.status}`);
+        } catch (e) {
+            const msg = e.message || e;
+            console.log(`Error while cancelling workflow_id ${workflow.id}: ${msg}`);
+        }
+    }
+    console.log('Done.');
+}
+
+main()
+    .then(() => core.info('Cancel Complete.'))
+    .catch(e => core.setFailed(e.message));
